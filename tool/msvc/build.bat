c3ae57ab96cd252a27f464c677bcf75e281e7e18
bynect <68197565+bynect@users.noreply.github.com>
2021-04-08 11:46:31 +0200

    Fix include path

--- tool/msvc/build.bat
+++ tool/msvc/build.bat
@@ -1,10 +1,10 @@
 ::use in Visual Studio Developer Command Prompt
 ::call tool/msvc/build.bat
 
-cl.exe src/*.c /I src /I include /EHsc /LD /Fosrc\ /Felib\jml /DEF tool/msvc/jml.def /link /out:lib/jml.dll
+cl.exe src/*.c /I include /EHsc /LD /Fosrc\ /Felib\jml /DEF tool/msvc/jml.def /link /out:lib/jml.dll
 
-cl.exe ext/cli/*.c lib/jml.lib /I src /I include /EHsc /Foext\cli\ /Feext\cli\jml /link /out:bin/jml.exe
+cl.exe ext/cli/*.c lib/jml.lib /I include /EHsc /Foext\cli\ /Feext\cli\jml /link /out:bin/jml.exe
 
 for /R std %%f in (*.c) do (
-    cl.exe %%f /I src /I include lib/jml.lib /EHsc /LD /Fostd\ /Fe%%~dpnf /link /out:%%~dpnf.dll
+    cl.exe %%f /I include lib/jml.lib /EHsc /LD /Fostd\ /Fe%%~dpnf /link /out:%%~dpnf.dll
 )
