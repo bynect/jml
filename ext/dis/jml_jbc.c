f169b1c809e3fe06e44ea6a65dfdf452e49eb2f3
bynect <68197565+bynect@users.noreply.github.com>
2021-04-16 14:56:45 +0200

    Implement basic bytecode disassembler

--- ext/dis/jml_jbc.c
+++ ext/dis/jml_jbc.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <jml.h>
 #include <jml/jml_serialization.h>
@@ -7,8 +8,28 @@
 int
 main(int argc, char **argv)
 {
-    (void) argc;
-    (void) argv;
-    printf("TODO\n");
-    return 0;
+    jml_vm_t *vm = jml_vm_new();
+    if (vm == NULL) return EXIT_FAILURE;
+
+    switch (argc) {
+        case 2: {
+            jml_bytecode_t bytecode;
+            if (!jml_deserialize_bytecode_file(&bytecode, argv[1])) {
+                printf("invalid bytecode file %s\n", argv[1]);
+                jml_bytecode_free(&bytecode);
+                return EXIT_FAILURE;
+            }
+
+            jml_bytecode_disassemble(&bytecode, argv[1]);
+            jml_bytecode_free(&bytecode);
+            break;
+        }
+
+        default:
+            printf("usage: %s [file]\n", argv[0]);
+            return EXIT_FAILURE;
+    }
+
+    jml_vm_free();
+    return EXIT_SUCCESS;
 }
