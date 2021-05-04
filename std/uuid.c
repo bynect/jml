2d539bcc71a1d8d6eac71f03755bf3a963740852
bynect <68197565+bynect@users.noreply.github.com>
2021-05-04 14:50:48 +0200

    Add windows support for uuid module

--- std/uuid.c
+++ std/uuid.c
@@ -7,6 +7,11 @@
 
 #include <uuid/uuid.h>
 
+#elif defined JML_PLATFORM_WIN
+
+#pragma comment(lib, "rpcrt4.lib")
+#include <rpcdce.h>
+
 #else
 
 #error "Current platform not supported."
@@ -23,14 +28,41 @@ jml_std_uuid_uuid(int arg_count, JML_UNUSED(jml_value_t *args))
     if (exc != NULL)
         return OBJ_VAL(exc);
 
+    const size_t length = 36;
+    char *buffer = jml_realloc(NULL, length + 1);
+
+#if defined JML_PLATFORM_NIX || defined JML_PLATFORM_MAC
     uuid_t uuid;
     uuid_generate_random(uuid);
 
-    char *buffer = jml_realloc(NULL, UUID_STR_LEN);
+    buffer = jml_realloc(NULL, length + 1);
     uuid_unparse(uuid, buffer);
 
+#elif defined JML_PLATFORM_WIN
+    UUID uuid;
+    char *temp;
+
+    RPC_STATUS status = UuidCreate(&uuid);
+    switch (status) {
+        case RPC_S_OK:
+        case RPC_S_UUID_LOCAL_ONLY:
+            UuidToStringA(&uuid, &temp);
+            memcpy(buffer, temp, length);
+            RpcStringFreeA(&temp);
+            break;
+
+        default:
+            exc = jml_obj_exception_format(
+                "WinRpcErr",
+                "Uuid creation failed, error code %d.",
+                status
+            );
+            return OBJ_VAL(exc);
+    }
+#endif
+
     return OBJ_VAL(
-        jml_obj_string_take(buffer, UUID_STR_LEN)
+        jml_obj_string_take(buffer, length)
     );
 }
 
--- tool/make/aux.py
+++ tool/make/aux.py
@@ -3,11 +3,11 @@
 import sys
 import re
 
-name = sys.argv[1].replace('.so', '.c', 1)
-with open(name, 'r') as f:
+name = sys.argv[1].replace(".so", ".c", 1)
+with open(name, "r") as f:
     lines = f.readline() + f.readline()
     pattern = re.compile(r"--\s*link\s*:\s*(\w+)", re.MULTILINE)
 
     for match in pattern.findall(lines):
-        if match != '':
-            print('-l' + match, end=' ')
+        if match != "":
+            print("-l" + match, end=" ")
