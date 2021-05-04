20b5f28c9efe69ab94382800b118d4abd066823a
bynect <68197565+bynect@users.noreply.github.com>
2021-05-04 10:39:39 +0200

    Add auxiliary script

---
+++ tool/make/aux.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+name = sys.argv[1].replace('.so', '.c', 1)
+with open(name, 'r') as f:
+    lines = f.readline() + f.readline()
+    pattern = re.compile(r"--\s*link\s*:\s*(\w+)", re.MULTILINE)
+
+    for match in pattern.findall(lines):
+        if match != '':
+            print('-l' + match, end=' ')
